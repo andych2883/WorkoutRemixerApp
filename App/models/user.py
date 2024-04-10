2ec21afc92afc5019c6f667cd405e2ab04ad818c
andych2883 <122915457+andych2883@users.noreply.github.com>
2024-04-10 19:00:32 -0400

    Revert "added: models and dataset"

--- App/models/user.py
+++ App/models/user.py
@@ -5,7 +5,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
-    routines = db.relationship('Routine', backref='user', lazy=True)
 
     def __init__(self, username, password):
         self.username = username
